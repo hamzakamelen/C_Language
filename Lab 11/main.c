535aa0026f23551c532927d65633b50c6b47878f
Hamza Kamelen <hamzakamelen12345@gmail.com>
2023-07-15 03:16:27 +0500

    Lab 11 Cont...

--- Lab 11/main.c
+++ Lab 11/main.c
@@ -1,3 +1,55 @@
+#include <stdio.h>
+//Task 1:   Write a function to calculate the factorial value of any integer entered through the keyboard.
+/*int facto(int n){
+if(n==0){
+    return 1;
+}else{
+return n*facto(n-1);
+}
+}*/
+//Task 2: Write a function power (a, b) to calculate the value of ‘a’ raised to the power of ‘b’.
+/*
+int power(int a,int b){
+if(b==0){
+    return 1;
+}else if(b==1){
+return a;
+}else{
+return a * power(a,b-1);
+}
+}
+*/
 int main(){
+//Task 1:   Write a function to calculate the factorial value of any integer entered through the keyboard.
+    /*int n,fact;
+    printf("Please Enter Factorial no: ");
+    scanf("%d",&n);
+    fact = facto(n);
+    printf("Factorial of %d is %d",n,fact);*/
+//Task 2: Write a function power (a, b) to calculate the value of ‘a’ raised to the power of ‘b’.
+ /*
+  int a, b;
+  printf("Enter the base: ");
+  scanf("%d", &a);
+  printf("Enter the exponent: ");
+  scanf("%d", &b);
+
+  int value = power(a, b);
+  printf("The value of %d raised to the power of %d is %d\n", a, b, value);
+  */
+/*Task 3:Write a general-purpose function to convert any given year into its roman equivalent. The following table shows the roman equivalents of decimal numbers:  
+Decimal          Roman                            
+1                   i
+100                 c 
+5                   v
+500                 d 
+10                  x 
+1000                m 
+50                  l
+https://www.youtube.com/watch?v=oocEj-8u4o8&t=21s
+Example:  
+Roman equivalent of 1988 is mdcccclxxxviii
+Roman equivalent of 1525 is mdxxv */
+
     return 0;
 }
