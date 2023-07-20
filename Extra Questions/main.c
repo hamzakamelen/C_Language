da3ce906e7244ea44ccdc31a6b4d65c8babbb002
Hamza Kamelen <hamzakamelen12345@gmail.com>
2023-07-20 23:37:23 +0500

    Completed

--- Extra Questions/main.c
+++ Extra Questions/main.c
@@ -91,3 +91,33 @@ int main() {
 
     return 0;
 }
+//============Palindrome==================
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(const char *str) {
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while (left < right) {
+        if (str[left] != str[right])
+            return 0;
+
+        left++;
+        right--;
+    }
+
+    return 1;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0'; // Removing trailing newline character
+    if (isPalindrome(str))
+        printf("Palindrome\n");
+    else
+        printf("Not a palindrome\n");
+    return 0;
+}
