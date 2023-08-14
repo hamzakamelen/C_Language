ce37f499762e4466871cfd1fe4502d40c72fa1d6
Hamza Kamelen <hamzakamelen12345@gmail.com>
2023-08-14 18:56:28 +0500

    Practice...

--- Extra Questions/pract.c
+++ Extra Questions/pract.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
     //---------------Transpose of a Matrix------------------
@@ -93,6 +94,7 @@ int main()
     // for(int i=1;i<=5;i++){
     //     printf("%d ",arr[i]);
     // }
+
     //--------------Bubble Sorting(Decending)---------------
     // int arr[5] = {1, 5, 3, 4, 2};
     // int temp = 0;
@@ -142,6 +144,87 @@ int main()
     // {
     //     printf("%d ", arr[i]);
     // }
+
+    //-----------------Array Reverse-------------------
+    // char Name[5]={'H','A','M','Z','A'};
+    // char RevName[5];
+    // int j=0,k;
+    // for(int i=5;i>=0;i--){
+    //     RevName[j] = Name[i];
+    //     j++;
+    // }
+
+    // for(k=0;k<6;k++){
+    //     printf("%c",RevName[k]);
+    // }
+
+    //-----------------Prime Numbers-------------------
+    // int n, count;
+    // for (n = 1; n <= 1000; n++)
+    // {
+    //     count = 0;
+    //     for (int i = 1; i <= n; i++)
+    //     {
+    //         if (n % i == 0)
+    //         {
+    //             count++;
+    //         }
+    //     }
+    //     if (count == 2)
+    //     {
+    //         printf("%d is a Prime Number\n", n);
+    //     }
+    // }
+
+    //--------------------Convert Year into Roman---------------------
+    // int year;
+    // printf("Enter a Year ");
+    // scanf("%d",&year);
+    // void CurrentYear(int YearConvert){
+    //     while(YearConvert){
+    //         if(YearConvert >=1000){
+    //             printf("M");
+    //             YearConvert = YearConvert-1000;
+    //         }else if(YearConvert>=500){
+    //             printf("D");
+    //             YearConvert = YearConvert-500;
+    //         }else if(YearConvert>=100){
+    //             printf("C");
+    //             YearConvert = YearConvert-100;
+    //         }else if(YearConvert>=50){
+    //             printf("L");
+    //             YearConvert = YearConvert-50;
+    //         }else if(YearConvert>=10){
+    //             printf("X");
+    //             YearConvert = YearConvert-10;
+    //         }else if(YearConvert>=5){
+    //             printf("V");
+    //             YearConvert = YearConvert-5;
+    //         }else if(YearConvert>=1){
+    //             printf("I");
+    //             YearConvert = YearConvert-1;
+    //         }
+    //     }
+    // }
+    // CurrentYear(year);
+
+    //-----------------Replace Character-------------------
+    // char Name[20];
+    // char replaceCh, With;
+    // printf("Please Enter your Name ");
+    // gets(Name);
+    // system('cls');
+    // printf("Enter a Character which you want to replace ");
+    // scanf(" %c",&replaceCh);
+    // printf("\nReplace with? ");
+    // scanf(" %c",&With);
+    // for(int i=0;i<strlen(Name);i++){
+    //     if(Name[i]==replaceCh){
+    //         Name[i] = With;
+    //     }
+    // }
+    // // Print After Replacing
+    // puts(Name);
 
     
     return 0;
